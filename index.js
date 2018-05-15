954e3ce338a9d2a46b333d7715dae3202e04b95f
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-15 05:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ function sayHiToGrandma(string) {
   {
     return "I can't hear you!"
   }
+  else if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
   else {
     return "YES INDEED!"
   }
